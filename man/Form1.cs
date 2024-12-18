f1a12c08e38c18da9d8680f6d0a2d611ee8a08d2
Erik Sundberg <matserik.sundberg@learnet.se>
2024-12-18 09:40:21 +0100

    Ghost added with a few bugs.

--- man/Form1.cs
+++ man/Form1.cs
@@ -2,14 +2,17 @@ namespace man
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
         int manX = 1;
         int manY = 1;
+        int ghostX = 3;
+        int ghostY = 4;
         int score = 0;
         int[,] level1 = {{1,1,1,1,1,1,1,1,1,1,1},
                          {1,2,3,3,3,3,3,3,3,3,1},
                          {1,3,1,1,1,3,1,1,1,3,1},
                          {1,3,1,3,3,3,3,3,1,3,1},
-                         {1,3,3,3,1,1,1,3,3,3,1},
+                         {1,3,3,4,1,1,1,3,3,3,1},
                          {1,3,1,3,3,3,3,3,1,3,1},
                          {1,3,1,1,1,3,1,1,1,3,1},
                          {1,3,3,3,3,3,3,3,3,3,1},
@@ -49,7 +52,12 @@ namespace man
                     {
                         SolidBrush brush = new SolidBrush(Color.White);
                         g.FillEllipse(brush, i * 32 + 8, j * 32 + 8, 16, 16);
-
+                    }
+                    else if (level1[j, i] == 4)
+                    {
+                        SolidBrush brush = new SolidBrush(Color.PaleGoldenrod);
+                        g.FillEllipse(brush, i * 32, j * 32, 32, 32);
+                        g.FillRectangle(brush, i * 32, j * 32 + 16, 32, 16);
                     }
                 }
             }
@@ -68,7 +76,7 @@ namespace man
                 {
                     manX++;
                 }
-                
+
             }
             else if (e.KeyCode == Keys.Left)
             {
@@ -79,7 +87,7 @@ namespace man
             }
             else if (e.KeyCode == Keys.Up)
             {
-                if (level1[manY-1, manX] != 1)
+                if (level1[manY - 1, manX] != 1)
                 {
                     manY--;
                 }
@@ -97,6 +105,31 @@ namespace man
             }
             level1[manY, manX] = 2;
 
+            Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int direction = random.Next(4);
+            level1[ghostY, ghostX] = 0;
+            if(direction==0)
+            {
+                ghostY--;
+            }
+            else if (direction == 1)
+            {
+                ghostX--;
+            }
+            else if (direction == 2)
+            {
+                ghostX++;
+            }
+            else if (direction == 3)
+            {
+                ghostY++;
+            }
+           
+            level1[ghostY, ghostX] = 4;
             Refresh();
         }
     }
